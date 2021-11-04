82c8fc1610bae5780c0d118222595feb007d2485
asalvero <ajalvero1@gmail.com>
2021-11-04 15:45:15 -0400

    Created User, UserDao, UserDaoFactory, and UserDaoImpli

--- src/main/java/org/example/UserDao.java
+++ src/main/java/org/example/UserDao.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import java.sql.SQLException;
+
 public interface UserDao {
+    void register(User user) throws SQLException;
+    User login(User user) throws SQLException;
+    void addToCart(int id) throws SQLException;
+    void purchaseOrCancel(User user, int choice) throws SQLException;
+    void selectCategory() throws SQLException;
+    void getMoreInfo() throws SQLException;
 }
--- src/main/java/org/example/UserDaoFactory.java
+++ src/main/java/org/example/UserDaoFactory.java
@@ -1,4 +1,16 @@
 package org.example;
 
 public class UserDaoFactory {
+    private static UserDao dao;
+
+    private UserDaoFactory(){
+
+    }
+
+    public static UserDao getUserDao(){
+        if(dao == null){
+            dao = new UserDaoImpl();
+        }
+        return dao;
+    }
 }
